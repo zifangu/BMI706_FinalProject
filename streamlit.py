d009230038ca08e74cd128a72523badae5ff89e4
wyc79 <yuanchen.wang79@outlook.com>
2022-04-16 19:32:57 -0400

    basic structure

--- streamlit.py
+++ streamlit.py
@@ -4,11 +4,17 @@ import pandas as pd
 import numpy as np
 
 def main():
-    st.sidebar.title("What to do")
-    app_mode = st.sidebar.selectbox("Choose the app mode",
-        ["Instructions", "Run the app", "Show the source code"])
+    st.sidebar.title("BMI 706 Final Project")
+    vis_mode = st.sidebar.selectbox("Choose mode and visualization",
+        ["Instructions", "Visualization 1", "Show the source code"])
+    if vis_mode == "Show instructions":
+        st.sidebar.success("Choose any visualization to view content.")
+    elif vis_mode == "Visualization 1":
+        run_vis_1()
 
+def run_vis_1():
 
+    return
 
 
 if __name__ == "__main__":
