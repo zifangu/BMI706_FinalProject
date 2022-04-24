f353ddcab33b92ce862e50f0bd6e96e02cd25c3d
qzhang21 <44728433+qzhang21@users.noreply.github.com>
2022-04-24 18:10:51 -0400

    Marie_try

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
+import os
 
 # https://github.com/streamlit/demo-self-driving
 
@@ -82,33 +83,26 @@ def run_vis_2():
     return
 
 def run_vis_3():
-    # time vs variables
-    lapse_name="lapse"
-    date_names=["ActivityDay", "SleepDay", "ActivityDate", "Date"]
+    processed_data_dir = "ProcessedData"
+    data_dir = "Data"
+    df = pd.read_csv(os.path.join(processed_data_dir,"dailyActivity_weight_merged.csv"),index_col=0)
+    df = date_lapse(df)
 
-    df_name = st.selectbox("Select Variable",["Calories", "Steps", "Sleep"])
-    # var = st.selectbox("Select Variable", list(data_dict.keys()))
-    if df_name == "Calories":
-        var = "Calories"
-    elif df_name == "Steps":
-        var = "StepTotal"
-    elif df_name == "Sleep":
-        var = st.selectbox(f"Variables in {df_name}", ["TotalMinutesAsleep", "TotalTimeInBed"])
+    data_level = st.selectbox("Select Level of Data",["Within Individual","Between Individuals"])
+    if data_level == "Within Individual":
+        x_vars = list(df.columns).remove('Id')
+    elif data_level == "Between Individuals":
+        x_vars = list(df.columns).remove('Id')
+    
+    x_var = st.selectbox("Select X variable",x_vars)
 
-    df = pd.read_csv(data_root + data_dict[df_name])
-    df = date_lapse(df, date_names=date_names, lapse_name=lapse_name)
+    y_vars = df.columns.remove(x_var)
 
-    chart = alt.Chart(df).mark_line().encode(
-        x=alt.X(lapse_name),
-        y=alt.Y(var),
-        color=alt.Color("Id", type="nominal"),
-        tooltip=[lapse_name, var],
-    ).properties(
-        title="hi",
-    )
+    y_var = st.selectbox("Select Y variable",y_vars)
+    
+    st.write(type(df.columns))
 
-    chart
-
+    
     return
 
 def date_lapse(df, date_names=["ActivityDay", "SleepDay", "ActivityDate", "Date"], lapse_name="lapse"):
